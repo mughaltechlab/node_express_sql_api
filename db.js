a8b0c43e0795337f7e40503c71267d6b22b954e0
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-05-25 11:28:10 +0500

    connect with my database and fetch table data

--- db.js
+++ db.js
@@ -1,21 +1,54 @@
 // const mysql = require('mysql2');
 import mysql from "mysql2"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+console.log(process.env.HOST);
+console.log(process.env.USERNAME);
+console.log(process.env.PASSWORD);
+console.log(process.env.DATABASE);
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+console.log('----------------------------------------')
+
+
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    // host: '127.0.0.1',
     // host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'easymeet'
+    // user: 'root',
+    // password: '',
+    // database: 'easymeet',
+
 }).promise()
+pool.query("SELECT * FROM user_types")
+.then(res => console.log(res))
 
-// pool.query("SELECT * FROM user_types")
-// .then(res => console.log(res[0]))
 
+
+// get all users
 async function getUsersType(){
     const result = await pool.query("SELECT * FROM user_types");
     return result;
 }
 
+// get user by id
+async function getUserById(id){
+    const result = await pool.query(`SELECT * FROM user_types WHERE id = ${id}`);
+    return result
+}
+
 const userType = await getUsersType();
-console.log(userType);
+const userTypeById = await getUserById(1);
+
+console.log(userTypeById);
